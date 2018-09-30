70c709dcbbbb447e186716b84da94272f83ae455
hyoj <chohyojoy@gmail.com>
2018-10-01 00:24:45 +0900

    Initial commit

---
+++ src/main/java/com/teamlck/smartanalyticsserver/SmartAnalyticsServerApplication.java
@@ -0,0 +1,12 @@
+package com.teamlck.smartanalyticsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAnalyticsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartAnalyticsServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/teamlck/smartanalyticsserver/SmartAnalyticsServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.teamlck.smartanalyticsserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartAnalyticsServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
